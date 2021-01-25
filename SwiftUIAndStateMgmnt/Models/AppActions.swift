398963dbaf78257b379642925311e6689fafb9e3
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-01-25 12:47:35 +0200

    Generated enum properties so we can access associated values via KeyPath.

--- SwiftUIAndStateMgmnt/Models/AppActions.swift
+++ SwiftUIAndStateMgmnt/Models/AppActions.swift
@@ -19,6 +19,17 @@ enum PrimeModalAction {
 
 enum FavoritePrimesAction {
     case deleteFavoritePrimes(IndexSet)
+
+    var deleteFavoritePrimes: IndexSet? {
+        get {
+            guard case let .deleteFavoritePrimes(value) = self else { return nil }
+            return value
+        }
+        set {
+            guard case .deleteFavoritePrimes = self, let newValue = newValue else { return }
+            self = .deleteFavoritePrimes(newValue)
+        }
+    }
 }
 
 // All the actions user can perform in the App
@@ -26,4 +37,37 @@ enum AppAction {
     case counter(CounterAction)
     case primeModal(PrimeModalAction)
     case favoritePrimes(FavoritePrimesAction)
+
+    var counter: CounterAction? {
+        get {
+            guard case let .counter(value) = self else { return nil }
+            return value
+        }
+        set {
+            guard case .counter = self, let newValue = newValue else { return }
+            self = .counter(newValue)
+        }
+    }
+
+    var primeModal: PrimeModalAction? {
+        get {
+            guard case let .primeModal(value) = self else { return nil }
+            return value
+        }
+        set {
+            guard case .primeModal = self, let newValue = newValue else { return }
+            self = .primeModal(newValue)
+        }
+    }
+
+    var favoritePrimes: FavoritePrimesAction? {
+        get {
+            guard case let .favoritePrimes(value) = self else { return nil }
+            return value
+        }
+        set {
+            guard case .favoritePrimes = self, let newValue = newValue else { return }
+            self = .favoritePrimes(newValue)
+        }
+    }
 }
