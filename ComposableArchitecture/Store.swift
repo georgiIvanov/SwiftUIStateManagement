0e4ee16eeff180cd89c94eba9b4646366be7d892
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-02-01 13:43:27 +0200

    Implement view method on Store that exposes some local value as a Store.

--- ComposableArchitecture/Store.swift
+++ ComposableArchitecture/Store.swift
@@ -5,9 +5,12 @@
 //  Created by Voro on 24.01.21.
 //
 
+import Combine
+
 public class Store<Value, Action>: ObservableObject {
     private let reducer: (inout Value, Action) -> Void
     @Published public private(set) var value: Value
+    private var storeUpdates: Cancellable?
     
     public init(initialValue: Value, reducer: @escaping (inout Value, Action) -> Void) {
         self.reducer = reducer
@@ -16,5 +19,18 @@ public class Store<Value, Action>: ObservableObject {
     
     public func send(_ action: Action) {
         reducer(&value, action)
+    }
+    
+    public func view<LocalValue>(_ f: @escaping (Value) -> LocalValue) -> Store<LocalValue, Action> {
+        let localStore = Store<LocalValue, Action>(initialValue: f(value)) { (localValue, action) in
+            self.send(action)
+            localValue = f(self.value)
+        }
+        
+        localStore.storeUpdates = $value.sink { [weak localStore] (newValue) in
+            localStore?.value = f(newValue)
+        }
+        
+        return localStore
     }
 }
