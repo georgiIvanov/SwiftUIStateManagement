bf5823a4f49851425afabcdb21d106a06584f956
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-02-10 20:50:14 +0200

    Unit tests for prime modal reducer.

--- PrimeModalTests/PrimeModalTests.swift
+++ PrimeModalTests/PrimeModalTests.swift
@@ -2,7 +2,21 @@ import XCTest
 @testable import PrimeModal
 
 class PrimeModalTests: XCTestCase {
-    func testExample() throws {
-        XCTAssert(true)
+    func testSaveFavoritePrimeTapped() throws {
+        var state = PrimeModalViewState(count: 2, favoritePrimes: [3, 5])
+        let effects = primeModalReducer(state: &state, action: .saveFavoritePrimeTapped)
+        
+        XCTAssertEqual(state.count, 2)
+        XCTAssertEqual(state.favoritePrimes, [3, 5, 2])
+        XCTAssert(effects.isEmpty)
+    }
+    
+    func testRemoveFavoritePrimeTapped() throws {
+        var state = PrimeModalViewState(count: 3, favoritePrimes: [3, 5])
+        let effects = primeModalReducer(state: &state, action: .removeFavoritePrimeTapped)
+        
+        XCTAssertEqual(state.count, 3)
+        XCTAssertEqual(state.favoritePrimes, [5])
+        XCTAssert(effects.isEmpty)
     }
 }
