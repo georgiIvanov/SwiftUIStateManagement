5f7ace9418a4e84de9c67687bbbfc96dccfe08c7
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-02-10 21:56:25 +0200

    Favorite primes reducer tests.

--- FavoritePrimesTests/FavoritePrimesTests.swift
+++ FavoritePrimesTests/FavoritePrimesTests.swift
@@ -9,25 +9,33 @@ import XCTest
 @testable import FavoritePrimes
 
 class FavoritePrimesTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testDeleteFavoritePrimes() throws {
+        var state = [2, 3, 5, 7]
+        let effects = favoritePrimesReducer(state: &state, action: .deleteFavoritePrimes([2]))
+        
+        XCTAssertEqual(state, [2, 3, 7])
+        XCTAssertTrue(effects.isEmpty)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testSaveButtonTapped() throws {
+        var state = [2, 3, 5, 7]
+        let effects = favoritePrimesReducer(state: &state, action: .saveButtonTapped)
+        
+        XCTAssertEqual(state, [2, 3, 5, 7])
+        XCTAssertEqual(effects.count, 1)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testLoadFavoritePrimesFlow() throws {
+        var state = [2, 3, 5, 7]
+        var effects = favoritePrimesReducer(state: &state, action: .loadButtonTapped)
+        
+        XCTAssertEqual(state, [2, 3, 5, 7])
+        XCTAssertEqual(effects.count, 1)
+        
+        effects = favoritePrimesReducer(state: &state, action: .loadedFavoritePrimes([2, 31]))
+        
+        XCTAssertEqual(state, [2, 31])
+        XCTAssert(effects.isEmpty)
     }
 
 }
