ef60a00ea4293454256f79e1df842d4761d33464
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-02-10 22:06:06 +0200

    Test for unhappy flow when nth prime button tapped. Test prime modal save / remove prime tap.

--- CounterTests/CounterTests.swift
+++ CounterTests/CounterTests.swift
@@ -41,7 +41,7 @@ class CounterTests: XCTestCase {
         XCTAssert(effects.isEmpty)
     }
     
-    func testNthPrimeButtonFlow() throws {
+    func testNthPrimeButtonHappyFlow() throws {
         var state = CounterViewState(count: 2,
                                      favoritePrimes: [3, 5],
                                      alertNthPrime: nil,
@@ -73,5 +73,57 @@ class CounterTests: XCTestCase {
                                         alertNthPrime: nil,
                                         isNthPrimeButtonDisabled: false))
         XCTAssert(effects.isEmpty)
+    }
+    
+    func testNthPrimeButtonUnhappyFlow() throws {
+        var state = CounterViewState(count: 2,
+                                     favoritePrimes: [3, 5],
+                                     alertNthPrime: nil,
+                                     isNthPrimeButtonDisabled: false)
+        
+        var effects = counterViewReducer(&state, .counter(.nthPrimeButtonTapped))
+        
+        XCTAssertEqual(state,
+                       CounterViewState(count: 2,
+                                        favoritePrimes: [3, 5],
+                                        alertNthPrime: nil,
+                                        isNthPrimeButtonDisabled: true))
+        XCTAssertEqual(effects.count, 1)
+        
+        effects = counterViewReducer(&state, .counter(.nthPrimeResponse(nil)))
+        
+        XCTAssertEqual(state,
+                       CounterViewState(count: 2,
+                                        favoritePrimes: [3, 5],
+                                        alertNthPrime: nil,
+                                        isNthPrimeButtonDisabled: false))
+        XCTAssert(effects.isEmpty)
+    }
+    
+    func testPrimeModal() throws {
+        var state = CounterViewState(count: 2,
+                                     favoritePrimes: [3, 5],
+                                     alertNthPrime: nil,
+                                     isNthPrimeButtonDisabled: false)
+        
+        var effects = counterViewReducer(&state, .primeModal(.saveFavoritePrimeTapped))
+        
+        XCTAssertEqual(state,
+                       CounterViewState(count: 2,
+                                        favoritePrimes: [3, 5, 2],
+                                        alertNthPrime: nil,
+                                        isNthPrimeButtonDisabled: false))
+        XCTAssert(effects.isEmpty)
+        
+        effects = counterViewReducer(&state, .primeModal(.removeFavoritePrimeTapped))
+        
+        XCTAssertEqual(state,
+                       CounterViewState(count: 2,
+                                        favoritePrimes: [3, 5],
+                                        alertNthPrime: nil,
+                                        isNthPrimeButtonDisabled: false))
+        XCTAssert(effects.isEmpty)
+        
+        
     }
 }
